b44d12dd64cf0af98b63c18f58dd58983c604308
andrescuello7 <andrescuellotrabajo@gmail.com>
2023-11-12 12:28:15 -0300

    add correction with method

--- src/users/usersController.ts
+++ src/users/usersController.ts
@@ -8,10 +8,9 @@ import { sign } from "jsonwebtoken";
 export async function getUser(req: Request, res: Response) {
   try {
     const response = await prisma.user.findFirstOrThrow({
-      where: {email: req.body}
+      where: { email: req.body.email }
     })
     res.status(200).send({ users: response });
-    console.log(response)
   } catch (error: any) {
     res.status(400).send({ error: GENERAL_ERROR });
   }
@@ -22,18 +21,10 @@ export async function postUsers(req: IRequest, res: Response) {
     if (await isExistingUser(req.body.email)) {
       throw new Error(GENERAL_ERROR);
     }
-    req.body.password = await cryptoEncode(req.body.password);
     const user = await prisma.user.create({
       data: req.body,
     });
-    // Implement Jwt Authentication
-    const result = await sign({
-      sub: {
-        userId: user?.id
-      },
-    }, process.env.SECRET!, { expiresIn: '3600' });
-    
-    res.status(200).send({ token: result });
+    res.status(200).send({ user: user });
   } catch (error: any) {
     console.log(error)
     res.status(400).send({ error: error.message ? error.message : GENERAL_ERROR });
