0a7fb0e8f039ecd5b03f9affbba0bf54db2a029c
agigli <agigli@iuvity.net>
2023-11-12 11:41:11 -0300

    [ADD] validation for an created user

--- src/users/usersController.ts
+++ src/users/usersController.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from "express";
 import { IRequest } from "src/interface";
 import prisma from "../../config/prisma";
+import { GENERAL_ERROR } from "../constants";
+import { isExistingUser } from "../utils";
 
 export async function getUser(req: Request, res: Response) {
   try {
-    const response = await prisma.user.findUnique({
+    const response = await prisma.user.findFirstOrThrow({
       where: {email: req.body}
     })
     res.status(200).send({ users: response });
+    console.log(response)
   } catch (error: any) {
     res.status(400).send({ error: "error GET" });
   }
@@ -15,12 +18,16 @@ export async function getUser(req: Request, res: Response) {
 
 export async function postUsers(req: IRequest, res: Response) {
   try {
+    if (await isExistingUser(req.body.email)) {
+      throw new Error(GENERAL_ERROR);
+    }
     const response = await prisma.user.create({
       data: req.body,
     });
     res.status(200).send({ users: response });
   } catch (error: any) {
-    res.status(400).send({ error: "error POST" });
+    console.log(error)
+    res.status(400).send({ error: error.message ? error.message : "Error POST" });
   }
 }
 export async function putUsers(req: Request, res: Response) {
--- src/users/usersRouter.ts
+++ src/users/usersRouter.ts
@@ -1,12 +1,11 @@
 import { Router } from "express";
 
-import { auth } from "../middewares/Authentication";
 import { deleteUsers, getUser, postUsers, putUsers } from "./usersController";
 
 const router: Router = Router();
 
 router.get("/", getUser);
-router.post("/", auth, postUsers);
+router.post("/", postUsers);
 router.put("/:id", putUsers);
 router.delete("/:id", deleteUsers);
 
